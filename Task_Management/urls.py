db8af7377d2a447f6e9200701710eefd0dae01e1
Nabil <nabilehsanul@gmail.com>
2026-05-09 18:41:00 +0600

    Task_Management_Update

--- Task_Management/urls.py
+++ Task_Management/urls.py
@@ -22,6 +22,6 @@ from tasks import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("hii/",views.hi),
-    path("", home ),
+    path("home", home ),
     path("tasks/",include("tasks.urls"))
 ]
