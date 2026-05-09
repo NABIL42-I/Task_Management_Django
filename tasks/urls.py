bb021beefd54109db25930187e46149a0c512f54
Nabil <nabilehsanul@gmail.com>
2026-05-09 18:13:38 +0600

    Update

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('show-task/',views.show)
+    path('show-task/',views.show),
     path('show-task/<int:id>/',views.nshow)
     # path('show-task/<int:id>', views.nshow)
 ]
